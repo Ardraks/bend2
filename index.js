559fdb9a8cc409ec53a2934860526497d5e9965d
Ardraks <127195188+Ardraks@users.noreply.github.com>
2024-01-22 12:19:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require("express")
+const cors=require("cors")
+const db=require("./connection/Database")
+
+
+const loginrouter=require('./routes/Loginroute')
+const writerouter=require('./routes/Writeroute')
+const updaterouter=require('./routes/Updateroute')
+
+
+const updatemodel = require('./model/Update')
+const loginmodel = require('./model/Login')
+const writemodel = require('./model/Write')
+
+const  app=new express();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(request,response)=>{
+    response.send("hai")
+})
+app.use("/login",loginrouter)
+app.use("/update",updaterouter)
+app.use("/write",writerouter)
+
+
+
+app.listen(4005,(request,response)=>{
+    console.log("port is running in 4005")
+})
---
+++ routes/Loginroute.js
@@ -0,0 +1,25 @@
+const loginmodel=require("../model/Login")
+const app = require('express').Router()
+
+
+app.post ('/loginview', async (request, response) => {
+    const { email, password } = request.body;
+    console.log(request.body)
+    try {
+      const user = await loginmodel.findOne({ email, password });
+    
+      if (user) {
+        response.json({ success: true, message: 'Login successful' });
+      }
+       else {
+        response.json({ success: false, message: 'Invalid Password and email' });
+      }
+    } catch (error) {
+      response.status(500).json({ success: false, message: 'Error during login' });
+    }
+  });
+
+
+
+
+  module.exports=app
